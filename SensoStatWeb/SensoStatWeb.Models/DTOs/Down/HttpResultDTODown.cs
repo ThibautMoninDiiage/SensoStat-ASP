7b9fe5a5242c71836428d00324ad782a11c30424
ThomasBernardPageUp <tbernard@pageup.fr>
2022-02-17 21:56:49 +0100

    Create http result

---
+++ SensoStatWeb/SensoStatWeb.Models/DTOs/Down/HttpResultDTODown.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SensoStatWeb.Models.DTOs.Down
+{
+    public class HttpResultDTODown
+    {
+        [JsonProperty("type")]
+        public string? Type { get; set; }
+
+        [JsonProperty("title")]
+        public string? Title { get; set; }
+
+        [JsonProperty("status")]
+        public int? Status { get; set; }
+
+        [JsonProperty("traceId")]
+        public string? TraceId { get; set; }
+    }
+}
+
