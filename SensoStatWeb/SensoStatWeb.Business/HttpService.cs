5b3edf0932b4b49d92b337decd61a1dfc0be85f7
ThomasBernardPageUp <tbernard@pageup.fr>
2022-02-17 21:30:10 +0100

    Add Http service to business

---
+++ SensoStatWeb/SensoStatWeb.Business/HttpService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace SensoStatWeb.Business
+{
+    public class HttpService
+    {
+        public async Task<T> SendHttpRequest<T>(string url, HttpMethod httpMethod, string? bearer = null)
+        {
+            var httpClient = new HttpClient();
+
+            // For OAuth2.0
+            if (!string.IsNullOrEmpty(bearer))
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearer);
+
+            var httpRequestMessage = new HttpRequestMessage() { Method = httpMethod, RequestUri = new Uri(url) };
+
+            var response = httpClient.SendAsync(httpRequestMessage);
+
+            if (response.Result.IsSuccessStatusCode)
+                return JsonConvert.DeserializeObject<T>(await response.Result.Content.ReadAsStringAsync());
+
+
+            return default(T);
+        }
+    }
+}
+
---
+++ SensoStatWeb/SensoStatWeb.Business/Interfaces/IHttpService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SensoStatWeb.Business.Interfaces
+{
+    public interface IHttpService
+    {
+        /// <summary>
+        /// Send a Http request
+        /// </summary>
+        /// <typeparam name="T">The class to deserialize the result</typeparam>
+        /// <param name="url">Url to send the http request</param>
+        /// <param name="httpMethod">The Http verb (HttpMethod.Get or HttpMethod.Post)</param>
+        /// <param name="bearer">The bearer token for OAuth2.0</param>
+        /// <returns>The deserialized result</returns>
+        Task<T> SendHttpRequest<T>(string url, HttpMethod httpMethod, string? bearer = null);
+    }
+}
+
