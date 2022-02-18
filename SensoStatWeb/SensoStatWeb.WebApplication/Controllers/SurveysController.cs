84aa5c39fb07187a9cd3ff720d46a8e8e2a9c5d2
ThomasBernardPageUp <tbernard@pageup.fr>
2022-02-18 16:18:13 +0100

    add 2 surveys

--- SensoStatWeb/SensoStatWeb.WebApplication/Controllers/SurveysController.cs
+++ SensoStatWeb/SensoStatWeb.WebApplication/Controllers/SurveysController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using SensoStatWeb.Models.Entities;
 using SensoStatWeb.WebApplication.ViewModels;
 
 namespace SensoStatWeb.WebApplication.Controllers
@@ -8,9 +9,11 @@ namespace SensoStatWeb.WebApplication.Controllers
     {
         public IActionResult Index()
         {
+            var survey = new Survey();
+
             var model = new SurveysViewModel
             {
-
+                Surveys = new List<Survey> { survey, survey}
             };
 
             return this.View(model);
