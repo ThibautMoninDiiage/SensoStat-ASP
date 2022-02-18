c9474af139932f83aa9acbe7fcea3ded01ce92a9
Tristan DEVOILLE <tristan.devoille@diiage.org>
2022-02-18 13:13:02 +0100

    feat : [CreateRepository] Ajout des repository pour administrator et survey + Creation du controller Survey + Méthode pour récuperer tous les surveys

---
+++ SensoStatWeb/SensoStatWeb.Repository/Interfaces/IAdministratorRepository.cs
@@ -0,0 +1,14 @@
+﻿using SensoStatApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensoStatWeb.Repository.Interfaces
+{
+    public interface IAdministratorRepository
+    {
+        public Administrator Login(string username, string password);
+    }
+}
