c2549c1aec60d1bd50d6ba59013c964ba07afe96
Baptiste RAMEAU <brameau@deliverup.com>
2022-02-19 20:25:54 +0100

    feat: [SurveyViewModel] Generate CreateSurveyViewModel + Add DataAnnotations to survey's name

---
+++ SensoStatWeb/SensoStatWeb.WebApplication/ViewModels/CreateSurveyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SensoStatWeb.WebApplication.ViewModels
+{
+	public class CreateSurveyViewModel
+	{
+        [Required(ErrorMessage = "Entrez le nom d'une séance"),
+         MinLength(2, "Le nom de la séance doit contenir au moins 2 caractères"),
+         MaxLength(15, "Le nom de la séance doit contenir un maximum de 15 caractères")]
+        public string Name { get; set; }
+
+        public string? ErrorMessage { get; set; }
+    }
+}
