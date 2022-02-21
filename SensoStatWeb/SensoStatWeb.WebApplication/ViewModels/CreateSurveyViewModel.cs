fd11ab3eff28b6df3643d184efa6cd9a533f8428
ThomasBernardPageUp <tbernard@pageup.fr>
2022-02-21 14:50:45 +0100

    Fix bug baptiste

--- SensoStatWeb/SensoStatWeb.WebApplication/ViewModels/CreateSurveyViewModel.cs
+++ SensoStatWeb/SensoStatWeb.WebApplication/ViewModels/CreateSurveyViewModel.cs
@@ -6,8 +6,8 @@ namespace SensoStatWeb.WebApplication.ViewModels
 	public class CreateSurveyViewModel
 	{
         [Required(ErrorMessage = "Entrez le nom d'une séance"),
-         MinLength(2, "Le nom de la séance doit contenir au moins 2 caractères"),
-         MaxLength(15, "Le nom de la séance doit contenir un maximum de 15 caractères")]
+         MinLength(2, ErrorMessage = "Le nom de la séance doit contenir au moins 2 caractères"),
+         MaxLength(15, ErrorMessage = "Le nom de la séance doit contenir un maximum de 15 caractères")]
         public string Name { get; set; }
 
         public string? ErrorMessage { get; set; }
