8369bf11cd0488ee7b195ddfdcd1a06e31bc7eb8
Baptiste RAMEAU <brameau@deliverup.com>
2022-02-22 13:33:58 +0100

    feat: [Question] create IQuestionRepository

---
+++ SensoStatWeb/SensoStatWeb.Repository/Interfaces/IQuestionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SensoStatWeb.Models.Entities;
+
+namespace SensoStatWeb.Repository.Interfaces
+{
+	public interface IQuestionRepository
+	{
+		List<Question> GetAllQuestions();
+
+		Task<Question>? CreateQuestion(Question question);
+
+		Task<bool>? DeleteQuestion(int id);
+	}
+}
