c66e9b7bbde764f57a7c7dca443ec89c0a732bf0
Baptiste RAMEAU <brameau@deliverup.com>
2022-02-23 11:23:15 +0100

    feat: [DTODown] create SurveyCreationDTODown

---
+++ SensoStatWeb/SensoStatWeb.Models/DTOs/Down/SurveyCreationDTODown.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using SensoStatWeb.Models.Entities;
+
+namespace SensoStatWeb.Models.DTOs.Down
+{
+	public class SurveyCreationDTODown
+	{
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("instructions")]
+        public List<Instruction> Instructions { get; set; }
+
+        [JsonProperty("questions")]
+        public List<Question> Questions { get; set; }
+
+        [JsonProperty("products")]
+        public List<Product> Products { get; set; }
+
+        [JsonProperty("adminId")]
+        public int AdminId { get; set; }
+
+        [JsonProperty("users")]
+        public List<User> Users { get; set; }
+    }
+}
+
