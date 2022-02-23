5a2f07070111935c68b9c9eb8e924656bd2dfc9b
ThomasBernardPageUp <tbernard@pageup.fr>
2022-02-23 14:53:29 +0100

    Create FileService

---
+++ SensoStatWeb/SensoStatWeb.Business/FileService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SensoStatWeb.Business.Interfaces;
+
+namespace SensoStatWeb.Business
+{
+    public class FileService : IFileService
+    {
+        public async Task<T> ReadCsvFile<T>(StreamReader stream)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return default(T);
+        }
+    }
+}
+
---
+++ SensoStatWeb/SensoStatWeb.Business/Interfaces/IFileService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SensoStatWeb.Business.Interfaces
+{
+    public interface IFileService
+    {
+        Task<T> ReadCsvFile<T>(StreamReader stream);
+    }
+}
+
