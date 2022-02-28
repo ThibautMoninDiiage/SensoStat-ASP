1ab165dc8fe33d5ae34b61f0948f63b5732e058d
Baptiste RAMEAU <brameau@deliverup.com>
2022-02-28 09:05:42 +0100

    fix: [API] use heberged API

--- SensoStatWeb/SensoStatWeb.WebApplication/Commons/Constants.cs
+++ SensoStatWeb/SensoStatWeb.WebApplication/Commons/Constants.cs
@@ -3,9 +3,6 @@ namespace SensoStatWeb.WebApplication.Commons
 {
     public class Constants
     {
-        // public const string BaseUrlApi = "https://appsensostatapi.azurewebsites.net/";
-        public const string BaseUrlApi = "https://localhost:7019/";
-
+        public const string BaseUrlApi = "https://appsensostatapi.azurewebsites.net/";
     }
 }
-
