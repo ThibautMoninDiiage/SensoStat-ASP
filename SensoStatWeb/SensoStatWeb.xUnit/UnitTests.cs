d2a44184cf70830e375ebf688c47cf7df2b59777
Thibaut Monin <Thibaut.Monin@diiage.org>
2022-03-02 10:45:16 +0100

    Useless tests

---
+++ SensoStatWeb/SensoStatWeb.xUnit/UnitTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+using Moq;
+using SensoStatWeb.Repository.Interfaces;
+using SensoStatWeb.Models.Entities;
+using System.Collections.Generic;
+using SensoStatWeb.Api.Controllers;
+
+namespace SensoStatWeb.xUnit
+{
+	public class UnitTests
+	{
+        public void UselessTest()
+        {
+            #region Arrange
+
+            var quatre = 4;
+            var deux = 2;
+
+            #endregion
+
+            #region Act
+
+            var total = deux + deux;
+
+            #endregion
+
+            #region Assert
+
+            Assert.Equal(quatre, total);
+
+            #endregion
+        }
+
+        public void ApiTests()
+        {
+            
+        }
+
+    }
+}
