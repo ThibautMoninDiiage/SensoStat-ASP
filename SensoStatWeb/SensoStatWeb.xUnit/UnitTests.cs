7cd97e4fd98a96ea83d51ded37f8617c9031ca48
ThibautMoninDev <device.thibaut@outlook.com>
2022-03-03 11:17:58 +0100

    Feat : Remove useless files / Adding tests

--- SensoStatWeb/SensoStatWeb.xUnit/UnitTests.cs
+++ SensoStatWeb/SensoStatWeb.xUnit/UnitTests.cs
@@ -5,11 +5,16 @@ using SensoStatWeb.Repository.Interfaces;
 using SensoStatWeb.Models.Entities;
 using System.Collections.Generic;
 using SensoStatWeb.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SensoStatWeb.xUnit
 {
 	public class UnitTests
 	{
+        public readonly Mock<SensoStatDbContext> _dbContext = new();
+        public Mock<IInstructionRepository> instructionMock = new Mock<IInstructionRepository>();
+
+        [Fact]
         public void UselessTest()
         {
             #region Arrange
@@ -32,9 +37,96 @@ namespace SensoStatWeb.xUnit
             #endregion
         }
 
-        public void ApiTests()
+        [Fact]
+        public void GetInstructions()
         {
-            
+            #region Arrange
+
+            InstructionController instructionController = new InstructionController(instructionMock.Object);
+
+            #endregion
+
+            #region Act
+
+            var getAllInstructions = instructionController.Instructions();
+
+            #endregion
+
+            #region Assert
+
+            Assert.IsType<NotFoundObjectResult>(getAllInstructions);
+
+            #endregion
+        }
+
+        [Fact]
+        public void CreateInstructionSuccess()
+        {
+            #region Arrange
+
+            InstructionController instructionController = new InstructionController(instructionMock.Object);
+            Instruction instruction = new Instruction();
+            instruction.Libelle = "Sentez le produit";
+            _dbContext.Setup(x => x.Instructions.Add(instruction));
+
+            #endregion
+
+            #region Act
+
+            var createInstruction = instructionController.Instruction(instruction);
+
+            #endregion
+
+            #region Assert
+
+            Assert.IsType<OkObjectResult>(createInstruction);
+            _dbContext.Verify(x => x.Add(It.IsAny<Instruction>()), Times.Once);
+
+            #endregion
+        }
+
+        [Fact]
+        public void DeleteInstructionSuccess()
+        {
+            #region Arrange
+
+            InstructionController instructionController = new InstructionController(instructionMock.Object);
+
+            #endregion
+
+            #region Act
+
+            var deleteInstruction = instructionController.Instruction(1);
+
+            #endregion
+
+            #region Assert
+
+            Assert.IsType<OkObjectResult>(deleteInstruction);
+
+            #endregion
+        }
+
+        [Fact]
+        public void DeleteInstructionFail()
+        {
+            #region Arrange
+
+            InstructionController instructionController = new InstructionController(instructionMock.Object);
+
+            #endregion
+
+            #region Act
+
+            var deleteInstruction = instructionController.Instruction(39037821);
+
+            #endregion
+
+            #region Assert
+
+            Assert.IsType<OkObjectResult>(deleteInstruction);
+
+            #endregion
         }
 
     }
