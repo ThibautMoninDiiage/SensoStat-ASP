e2eb0d932c11a8c0b443cd58014d83ed7acee230
Baptiste RAMEAU <brameau@deliverup.com>
2022-03-03 16:51:46 +0100

    fix: [DeleteSurvey] survey.js + Index.html

--- SensoStatWeb/SensoStatWeb.WebApplication/wwwroot/scripts/surveys.js
+++ SensoStatWeb/SensoStatWeb.WebApplication/wwwroot/scripts/surveys.js
@@ -1,14 +1,20 @@
-﻿function showPopupDeleteSurvey() {
-    const deleteButton = new Button("Supprimer", "red", deleteSurvey); // If click call delete survey
-    const cancelButton = new Button("Annuler", "blue", enableWindow); // If click close the pop up
+﻿function showPopupDeleteSurvey(id) {
+  const deleteButton = new Button("Supprimer", "red", () => deleteSurvey(id)); // If click call delete survey
+  const cancelButton = new Button("Annuler", "blue", enableWindow); // If click close the pop up
 
+  var text = "Êtes vous certains de vouloir supprimer la séance.";
+  const alerte = new Alert("Confirmation suppression", text, "white", [
+    deleteButton,
+    cancelButton,
+  ]);
 
-    var text = "Êtes vous certains de vouloir supprimer la séance.";
-    const alerte = new Alert("Confirmation suppression", text, "white", [deleteButton, cancelButton]);
-
-    new window.alert(alerte);
+  new window.alert(alerte);
 }
 
-function deleteSurvey() {
-    enableWindow();
+function deleteSurvey(id) {
+  console.log(id);
+
+  fetch("https://localhost:7290/Survey/DeleteSurvey?id=" + id, { method: 'DELETE' });
+
+  enableWindow();
 }
