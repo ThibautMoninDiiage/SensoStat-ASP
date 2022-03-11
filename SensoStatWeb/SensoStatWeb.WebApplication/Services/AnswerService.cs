1cbe0e140184ba37f8ee01d1d8089986836369c6
Thomas BERNARD <tbernard@pageup.fr>
2022-03-11 10:23:11 +0100

    Create answer service for BO

--- SensoStatWeb/SensoStatWeb.WebApplication/Services/AnswerService.cs
+++ SensoStatWeb/SensoStatWeb.WebApplication/Services/AnswerService.cs
@@ -1,13 +1,23 @@
-﻿using SensoStatWeb.Models.DTOs.Down;
+﻿using SensoStatWeb.Business.Interfaces;
+using SensoStatWeb.Models.DTOs.Down;
+using SensoStatWeb.WebApplication.Commons;
 using SensoStatWeb.WebApplication.Services.Interfaces;
 
 namespace SensoStatWeb.WebApplication.Services
 {
     public class AnswerService : IAnswerService
     {
+        private readonly IHttpService _httpService;
+        public AnswerService(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+
         public async Task<IEnumerable<SurveyAnswersDTODown>> GetSurveyAnswers(int surveyId, string token)
         {
-            throw new NotImplementedException();
+            var answers = await _httpService.SendHttpRequest<IEnumerable<SurveyAnswersDTODown>>($"{Constants.BaseUrlApi}answer?surveyId={surveyId}", HttpMethod.Get, bearer:token);
+
+            return answers;
         }
     }
 }
