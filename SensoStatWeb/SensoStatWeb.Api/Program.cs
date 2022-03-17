fe8f78ef658ecfbfbec8ac946ab759e1278a33c8
Thomas BERNARD <tbernard@pageup.fr>
2022-03-17 09:23:07 +0100

    Delete question service

--- SensoStatWeb/SensoStatWeb.Api/Program.cs
+++ SensoStatWeb/SensoStatWeb.Api/Program.cs
@@ -104,7 +104,6 @@ builder.Services.AddScoped<IAnswerRepository, DbAnswerRepository>();
 //Injection de dépendances pour les services
 builder.Services.AddScoped<IAdministratorServices, AdministratorServices>();
 builder.Services.AddScoped<ISurveyServices, SurveyServices>();
-builder.Services.AddScoped<IQuestionServices, QuestionServices>();
 builder.Services.AddScoped<IUserServices, UserServices>();
 builder.Services.AddScoped<ISurveyStateServices, SurveyStateServices>();
 builder.Services.AddScoped<IProductServices, ProductServices>();
