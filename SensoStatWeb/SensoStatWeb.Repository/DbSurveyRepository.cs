d8a6f721ef0e00ec182df6f57e486cfe065d0e92
Thomas BERNARD <tbernard@pageup.fr>
2022-03-17 08:39:06 +0100

    remove useless usings

--- SensoStatWeb/SensoStatWeb.Repository/DbSurveyRepository.cs
+++ SensoStatWeb/SensoStatWeb.Repository/DbSurveyRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SensoStatWeb.Models.Entities;
 using SensoStatWeb.Repository.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SensoStatWeb.Repository
 {
