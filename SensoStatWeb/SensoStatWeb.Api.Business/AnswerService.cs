13e1446173f168da2311c6b6320857613fb78c26
Baptiste RAMEAU <baptiste.rameau@diiage.org>
2022-03-22 09:45:14 +0100

    feat: [AnswerService] method CreateAnswer()

--- SensoStatWeb/SensoStatWeb.Api.Business/AnswerService.cs
+++ SensoStatWeb/SensoStatWeb.Api.Business/AnswerService.cs
@@ -1,5 +1,8 @@
 ﻿using SensoStatWeb.Api.Business.Interfaces;
+using SensoStatWeb.Business.Interfaces;
 using SensoStatWeb.Models.DTOs.Down;
+using SensoStatWeb.Models.DTOs.Up;
+using SensoStatWeb.Models.Entities;
 using SensoStatWeb.Repository.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -12,10 +15,20 @@ namespace SensoStatWeb.Api.Business
     public class AnswerService : IAnswerService
     {
         private readonly IAnswerRepository _answerRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IProductServices _productService;
+        private readonly IJwtService _jwtService;
+        private readonly IUserServices _userService;
+        private readonly IQuestionRepository _questionRepository;
 
-        public AnswerService(IAnswerRepository answerRepository)
+        public AnswerService(IAnswerRepository answerRepository, IUserRepository userRepository, IProductServices productService, IJwtService jwtService, IUserServices userServices, IQuestionRepository questionRepository)
         {
             _answerRepository = answerRepository;
+            _userRepository = userRepository;
+            _productService = productService;
+            _jwtService = jwtService;
+            _userService = userServices;
+            _questionRepository = questionRepository;
         }
 
         public async Task<IEnumerable<SurveyAnswersDTODown>> GetSurveyAnswers(int surveyId)
@@ -25,6 +38,35 @@ namespace SensoStatWeb.Api.Business
             var result = answers.Select(a => new SurveyAnswersDTODown() { Answer = a.UserAnswer, Question = a.Question.Libelle, UserCode = a.User.Code });
 
             return result;
+        }
+
+        public async Task<Answer> CreateAnswer(AnswerDTOUp answerDTOUp)
+        {
+            var jsonToken = await _jwtService.ReadJwtToken(answerDTOUp.Token);
+            var userId = jsonToken.Claims.FirstOrDefault(c => c.Type.Contains("id")).Value;
+            var surveyId = jsonToken.Claims.FirstOrDefault(c => c.Type.Contains("primarysid")).Value;
+
+            var result = userId.Remove(userId.Length - surveyId.Length);
+
+            // .Substring(jsonToken.Claims.FirstOrDefault(c => c.Type.Contains("primarysid")).Value.Length);
+
+            var answer = new Answer()
+            {
+                UserId = result,
+                QuestionId = answerDTOUp.QuestionId,
+                UserAnswer = answerDTOUp.UserAnswer,
+                ProductId = answerDTOUp.ProductId,
+                User = await _userService.GetUser(userId),
+                Question = await _questionRepository.GetQuestion(answerDTOUp.QuestionId),
+
+            };
+            var internetDeFesse = await _productService.GetProduct(answerDTOUp.ProductId);
+            answer.Product = internetDeFesse;
+
+
+            var answerResult = await _answerRepository.CreateAnswer(answer);
+
+            return answerResult;
         }
     }
 }
