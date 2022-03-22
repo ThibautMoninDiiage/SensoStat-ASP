c1513f5529ee221776642a6560ae643cd71e464c
Baptiste RAMEAU <baptiste.rameau@diiage.org>
2022-03-22 09:44:08 +0100

    feat: [AnswerDTOUp] create the file

---
+++ SensoStatWeb/SensoStatWeb.Models/DTOs/Up/AnswerDTOUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SensoStatWeb.Models.DTOs.Up
+{
+	public class AnswerDTOUp
+	{
+		public string UserAnswer { get; set; }
+        public int QuestionId { get; set; }
+        public string Token { get; set; }
+        public int ProductId { get; set; }
+    }
+}
