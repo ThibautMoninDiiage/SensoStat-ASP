dcd03c205d6e800e334fd7a09156ac00c2df1b59
Baptiste RAMEAU <baptiste.rameau@diiage.org>
2022-03-22 09:46:37 +0100

    feat: [AnswerController] method CreateAnswer()

--- SensoStatWeb/SensoStatWeb.Api/Controllers/AnswerController.cs
+++ SensoStatWeb/SensoStatWeb.Api/Controllers/AnswerController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SensoStatWeb.Api.Business.Interfaces;
+using SensoStatWeb.Models.DTOs.Up;
+using SensoStatWeb.Models.Entities;
 
 namespace SensoStatWeb.Api.Controllers
 {
@@ -17,11 +19,20 @@ namespace SensoStatWeb.Api.Controllers
 
         [HttpGet]
         [Authorize]
-        public async Task<IActionResult> Answers(int surveyId)
+        [ActionName("Answer")]
+        public async Task<IActionResult> GetAnswers(int surveyId)
         {
             var answers = await _answerService.GetSurveyAnswers(surveyId);
 
             return Ok(answers);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAnswer([FromBody]AnswerDTOUp answerDTOUp)
+        {
+            var postAnswer = await _answerService.CreateAnswer(answerDTOUp);
+
+            return postAnswer == null ? NotFound() : Ok(postAnswer);
         }
     }
 }
