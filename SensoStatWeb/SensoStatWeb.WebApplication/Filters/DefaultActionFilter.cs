3ed6e5f1e6f25982a74b96a6b78490f18eb4f055
Baptiste RAMEAU <baptiste.rameau@diiage.org>
2022-03-23 11:10:29 +0100

    feat: [FILTER] add comments on all the code

--- SensoStatWeb/SensoStatWeb.WebApplication/Filters/DefaultActionFilter.cs
+++ SensoStatWeb/SensoStatWeb.WebApplication/Filters/DefaultActionFilter.cs
@@ -5,16 +5,25 @@ namespace SensoStatWeb.WebApplication.Filters
 {
     public class DefaultActionFilter : IActionFilter, IExceptionFilter
     {
+        #region OnActionExecuted()
+        /// <summary>
+        /// A user need to have a token for each page even on the login and the error page
+        /// </summary>
+        /// <param name="context"></param>
         public void OnActionExecuted(ActionExecutedContext context)
         {
+            // If the routes are not going on "/" and "/login" and "/Error"
             if(context.HttpContext.Request.Path.Value != "/" 
                 && !context.HttpContext.Request.Path.Value.Contains("/login") 
                 && !context.HttpContext.Request.Path.Value.Contains("/Error")
                 )
             {
+                // Get the Token from the Cookies
                 var token = context.HttpContext?.Request?.Cookies?.FirstOrDefault(x => x.Key == "Token").Value;
 
+                // If the token is null
                 if (token == null)
+                    // Redirect to route login
                     context.Result = new RedirectToRouteResult(new RouteValueDictionary(new
                     {
                         controller = "login", 
@@ -22,11 +31,15 @@ namespace SensoStatWeb.WebApplication.Filters
                     }));
             }
         }
+        #endregion
 
+        #region OnActionExecuting()
         public void OnActionExecuting(ActionExecutingContext context)
         {
         }
+        #endregion
 
+        #region OnException
         public void OnException(ExceptionContext context)
         {
             Console.WriteLine("======= ERROR =======");
@@ -35,5 +48,6 @@ namespace SensoStatWeb.WebApplication.Filters
 
             Console.WriteLine("=====================");
         }
+        #endregion
     }
 }
