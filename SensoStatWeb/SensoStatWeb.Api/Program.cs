e6029f272046eb9a4b4bb337413c79301223479e
Baptiste RAMEAU <baptiste.rameau@diiage.org>
2022-03-23 14:05:25 +0100

    hotfix

--- SensoStatWeb/SensoStatWeb.Api/Program.cs
+++ SensoStatWeb/SensoStatWeb.Api/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
