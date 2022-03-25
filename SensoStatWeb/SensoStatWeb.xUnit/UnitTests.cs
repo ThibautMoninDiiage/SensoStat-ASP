2e588ef3391e06822c2bae75877f84be186880a7
ThibautMonin <thibaut.monin@diiage.org>
2022-03-25 20:55:56 +0100

    On a enfin un test qui fonctionne correctement

--- SensoStatWeb/SensoStatWeb.xUnit/UnitTests.cs
+++ SensoStatWeb/SensoStatWeb.xUnit/UnitTests.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
 using Moq;
 using SensoStatWeb.Api.Business.Interfaces;
-using SensoStatWeb.Models.Entities;
 using System.Collections.Generic;
 using SensoStatWeb.Api.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using SensoStatWeb.Models.DTOs.Down;
 
 namespace SensoStatWeb.xUnit
 {
 	public class UnitTests
 	{
-        #region Variables
-
-        private readonly Mock<SensoStatDbContext> _dbContext = new();
-
-        #endregion
-
         #region UselessTest
 
         [Fact]
@@ -39,6 +33,62 @@ namespace SensoStatWeb.xUnit
 
             Assert.Equal(quatre, total);
 
+            #endregion
+        }
+
+        #endregion
+
+        #region AnswerTest
+
+        #region DataSample
+        private IEnumerable<SurveyAnswersDTODown> GetAnswers()
+        {
+            List<SurveyAnswersDTODown> answers = new()
+            {
+                new SurveyAnswersDTODown
+                {
+                    Answer = "Non",
+                    Question = "Aimez-vous les tests unitaires ?",
+                    UserCode = "001"
+                },
+                new SurveyAnswersDTODown
+                {
+                    Answer = "Oui",
+                    Question = "Aimez-vous Javascript ?",
+                    UserCode = "002"
+                },
+                new SurveyAnswersDTODown
+                {
+                    Answer = "Non",
+                    Question = "Aimez-vous Java ?",
+                    UserCode = "003"
+                }
+            };
+
+            return answers;
+        }
+        #endregion
+
+        #region Variables
+        private readonly Mock<IAnswerService> _answerService = new();
+        #endregion
+
+        [Fact]
+        public void GetAnswer_ListOfAnswer_AnswerExists()
+        {
+            #region Arrange
+            _answerService.Setup(a => a.GetSurveyAnswers(18)).Returns(Task.FromResult(GetAnswers()));
+            AnswerController answerController = new(_answerService.Object);
+            #endregion
+
+            #region Act
+            var actionResult = answerController.GetAnswers(18);
+            var result = actionResult.Result as OkObjectResult;
+            var actual = result.Value as IEnumerable<SurveyAnswersDTODown>;
+            #endregion
+
+            #region Assert
+            Assert.IsType<OkObjectResult>(result);
             #endregion
         }
 
