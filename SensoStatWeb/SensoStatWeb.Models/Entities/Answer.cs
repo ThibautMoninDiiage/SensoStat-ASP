43b8ca0fd4f7a1875c4260f1f81dc68974abab2e
ThomasBernardPageUp <tbernard@pageup.fr>
2022-03-28 17:08:39 +0200

    add pk

--- SensoStatWeb/SensoStatWeb.Models/Entities/Answer.cs
+++ SensoStatWeb/SensoStatWeb.Models/Entities/Answer.cs
@@ -7,14 +7,17 @@ namespace SensoStatWeb.Models.Entities
     [Table("Answer")]
     public class Answer
     {
+        [Key]
         public string? UserId { get; set; }
         public User? User { get; set; }
 
+        [Key]
         public int QuestionId { get; set; }
         public Question? Question { get; set; }
 
         public string? UserAnswer { get; set; }
 
+        [Key]
         public int ProductId { get; set; }
         public Product? Product { get; set; }
     }
