998c1899e213348e36bacccae011403711d9a6a3
Thomas BERNARD <thomas.bernard@diiage.org>
2022-03-31 11:34:22 +0000

    Updated LoginController.cs

--- SensoStatWeb/SensoStatWeb.WebApplication/Controllers/LoginController.cs
+++ SensoStatWeb/SensoStatWeb.WebApplication/Controllers/LoginController.cs
@@ -40,6 +40,7 @@ namespace SensoStatWeb.WebApplication.Controllers
         /// <returns></returns>
         public async Task<IActionResult> Authenticate(LoginViewModel loginViewModel)
         {
+            // TODO => Must delete during client presentation
             throw new System.Exception("Erreur login");
             // If the model state are not valid
             if (!ModelState.IsValid)
